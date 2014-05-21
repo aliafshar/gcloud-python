321dab3fc176dbc8d9cc5a0739e79373559782b7
Ali Afshar <afshar@google.com>
2014-05-21 14:54:24 -0700

    API library specific connections.

--- gcloud/connection.py
+++ gcloud/connection.py
@@ -42,3 +42,31 @@ class Connection(object):
         self._http = self._credentials.authorize(self._http)
     return self._http
 
+
+class ApiClientConnection(Connection):
+  """A generic connection that uses google-api-python-client."""
+
+  API_NAME = None
+  API_VERSION = None
+
+  _service = None
+
+  @property
+  def service(self):
+    """A getter for the api client service for making requests.
+
+    This service is built once from discovery and cached. Unsetting
+    :attr:`ApiClientConnection._service` will force a rebuild.
+
+    :rtype: :class:`apiclient.discovery.Resource`
+    :returns: an api client service
+    """
+    if not self._service:
+      if not (self.API_NAME and self.API_VERSION):
+        raise NotImplementedError(
+            'API_NAME and API_VERSION must be set for ApiConnections.')
+      from apiclient.discovery import build
+      self._service = build(self.API_NAME, self.API_VERSION, http=self.http)
+    return self._service
+
+
