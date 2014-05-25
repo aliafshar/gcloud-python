440e9cbbb9e4a271788feccc99d216248c334828
Ali <afshar@google.com>
2014-05-25 11:42:44 -0700

    Documented shortcut methods.

--- gcloud/dns/__init__.py
+++ gcloud/dns/__init__.py
@@ -3,17 +3,59 @@
 from gcloud.credentials import Credentials
 from gcloud.dns.connection import Connection
 
-#: Auth scope used for DNS
+#: Version of this package.
+__version__ = '0.1'
+
+#: Auth scope used for DNS.
 DNS_SCOPE = 'https://www.googleapis.com/auth/ndev.clouddns.readwrite'
 
 
 def get_connection(project, email, private_key_path, scopes=None):
+  """Shortcut for establishing a connection with Cloud DNS.
+
+  :type project: string
+  :param project: The name of the project to connect to.
+
+  :type client_email: string
+  :param client_email: The e-mail attached to the service account.
+
+  :type private_key_path: string
+  :param private_key_path: The path to a private key file (this file was
+                           given to you when you created the service
+                           account).
+
+  :rtype: :class:`gcloud.dns.connection.Connection`
+  :returns: A connection defined with the proper credentials.
+  """
+  scopes = scopes or DNS_SCOPE
   credentials = Credentials.get_for_service_account(
-      email, private_key_path, DNS_SCOPE)
+      email, private_key_path, scopes)
   return Connection(credentials=credentials, project=project)
 
-def get_zone(name, project_id, email, private_key_path):
-  c = get_connection(email, private_key_path, project_id)
-  return c.get_zone(name)
 
+def get_zone(zone, project, email, private_key_path):
+  """Shortcut for fetching a managed zone from Cloud DNS.
+
+  If you already have a zone, this function will likely be your quickest entry
+  point.
+
+  :type zone: string
+  :param zone: The name of the managed zone to fetch.
+
+  :type project: string
+  :param project: The name of the project to connect to.
+
+  :type client_email: string
+  :param client_email: The e-mail attached to the service account.
+
+  :type private_key_path: string
+  :param private_key_path: The path to a private key file (this file was
+                           given to you when you created the service
+                           account).
+
+  :rtype: :class:`gcloud.dns.resources.Zone`
+  :returns: A connection defined with the proper credentials.
+  """
+  connection = get_connection(project, email, private_key_path)
+  return connection.get_zone(zone)
 
