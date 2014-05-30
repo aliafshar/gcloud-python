0d60829533e0d923e3cd05299071967e30f0ecab
Ali Afshar <afshar@google.com>
2014-05-30 15:22:43 -0700

    Testing helpers.

--- tests/utils.py
+++ tests/utils.py
@@ -1,12 +1,25 @@
 
+import json
+
 
 class DummyRequest(object):
   """Helper to represent an HTTP request."""
 
-  def __init__(self, uri, method, body):
+  def __init__(self, uri, method, body, headers):
     self.uri = uri
     self.method = method
     self.body = body
+    self.headers = headers or {}
+
+class DummyResponse(object):
+  """Helper to represent an HTTP response."""
+
+  def __init__(self, status, body):
+    self.status = status
+    if isinstance(body, str):
+      self.body = body
+    else:
+      self.body = json.dumps(body)
 
 
 class HttpRecorder(object):
@@ -17,7 +30,7 @@ class HttpRecorder(object):
   1. It responds to requests with a sequence of responses that have been predefined.
   2. It records all requests for asserting correct parameters, urls, bodies etc.
 
-  >>> r = HttpRecorder([({'status': '200'}, 'Hello world')])
+  >>> r = HttpRecorder([DummyResponse(200, 'Hello world')])
   >>> r.request('http://google.com')
   {'status': '200'}, 'Hello World'
   >>> assert r.requests[0]
@@ -36,9 +49,9 @@ class HttpRecorder(object):
     self.responses = responses or self.responses or []
     self.requests = []
 
-  def request(self, uri, method='GET', body=None, redirections=1, connection_type=None):
+  def request(self, uri, method='GET', body=None, headers=None, redirections=1, connection_type=None):
     """This method stubs out httplib2.Http.request."""
-    self.requests.append(DummyRequest(uri, method, body))
+    self.requests.append(DummyRequest(uri, method, body, headers))
     try:
       response = self.responses[self.request_index]
     except IndexError:
